0bdceb56dc6d7a18003368ac8fee8691089a636c
Nhinguyen <238nhinguyen@gmail.com>
2025-09-30 20:51:49 +0700

    cbi deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,9 +3,11 @@ import taskRoutes from './routes/tasksRoutes.js';
 import { connectDB } from './config/db.js';
 import dotenv from 'dotenv';
 import cors from 'cors'
+import path from 'path'
 
 dotenv.config(); // load environment variables from .env file
 const PORT = process.env.PORT || 5001
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -19,10 +21,14 @@ auth: kiểm tra token người dùng hợp lệ > nếu hợp lệ > cho phép 
 */
 
 if (process.env.NODE_ENV !== "production") {
-  app.use(cors({ origin: "http://localhost:5173" }));
+ app.use(cors({ origin: "http://localhost:5173" }));
 }
-
-app.use('/api/tasks', taskRoutes);
+if (process.env.NODE_ENV === "production") {
+  app.use('/api/tasks', taskRoutes);
+app.use(express.static(path.join(__dirname,"../frontend/dist")));
+app.get("*",(req,res) => {
+  res.sendFile(path.join(__dirname,"../frontend/dist/index.html"))
+}) }
 
 connectDB().then(() => {
   app.listen(PORT, () => {
